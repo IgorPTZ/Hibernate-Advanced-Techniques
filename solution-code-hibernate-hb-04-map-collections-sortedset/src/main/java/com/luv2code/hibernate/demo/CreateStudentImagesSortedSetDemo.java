3e2e4fde066232c61b475e4abcb95ec397912906
Igor <igor.ptz@hotmail.com>
2022-01-01 11:06:21 -0300

    Update

---
+++ solution-code-hibernate-hb-04-map-collections-sortedset/src/main/java/com/luv2code/hibernate/demo/CreateStudentImagesSortedSetDemo.java
@@ -0,0 +1,63 @@
+package com.luv2code.hibernate.demo;
+
+import java.util.Set;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class CreateStudentImagesSortedSetDemo {
+	
+	public static void main(String[] args) {
+		
+		// Create session factory
+		SessionFactory factory = new Configuration()
+				                     .configure("hibernate.cfg.xml")
+				                     .addAnnotatedClass(Student.class)
+				                     .buildSessionFactory();
+		
+		// Create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// Create the object
+			Student tempStudent = new Student("John", "Doe", "paul@luv2code.com");
+			
+			Set<String> theImages = tempStudent.getImages();
+			
+			theImages.add("photo1.jpg");
+			
+			theImages.add("photo2.jpg");
+			
+			theImages.add("photo3.jpg");
+			
+			theImages.add("photo4.jpg");
+			
+			theImages.add("photo4.jpg"); // Duplicate, filtered at Java level by HashSet
+			
+			theImages.add("photo5.jpg");
+			
+			// Start transaction
+			session.beginTransaction();
+			
+			// Save the object
+			System.out.println("Saving the student and images...");
+			
+			session.persist(tempStudent);
+			
+			// Commit the transaction
+			session.getTransaction().commit();
+			
+			System.out.print("Done!");
+		}
+		finally {
+			
+			session.close();
+			
+			factory.close();
+			// Clean up code
+		}
+	}
+}
---
+++ solution-code-hibernate-hb-04-map-collections-sortedset/src/main/java/com/luv2code/hibernate/demo/GetStudentImagesSortedDemo.java
@@ -0,0 +1,49 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class GetStudentImagesSortedDemo {
+	
+	public static void main(String[] args) {
+		
+		// Create session factory
+		SessionFactory factory = new Configuration()
+				                .configure("hibernate.cfg.xml")
+				                .addAnnotatedClass(Student.class)
+				                .buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// Start a transaction
+			session.beginTransaction();
+			
+			// Get the student id
+			int id = 1;
+			
+			Student student = session.get(Student.class, id);
+			
+			// Print the student detail
+			System.out.println("Student details: " + student);
+			
+			// Print the associated images
+			System.out.println("The associated images: " + student.getImages());
+			
+			// Commit the transaction
+			session.getTransaction().commit();
+			
+			// Done
+			System.out.println("Done!");
+		}
+		finally {
+			// Close the transaction
+			session.close();
+			
+			factory.close();
+		}
+	}
+}
