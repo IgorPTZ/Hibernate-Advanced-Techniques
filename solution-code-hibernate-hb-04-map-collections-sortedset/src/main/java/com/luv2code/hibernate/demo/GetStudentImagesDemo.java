0c8bcc74e2c15986672e88a724e6ba6fa3317e1c
Igor <igor.ptz@hotmail.com>
2022-01-07 19:17:27 -0300

    Update

--- solution-code-hibernate-hb-04-map-collections-sortedset/src/main/java/com/luv2code/hibernate/demo/GetStudentImagesDemo.java
+++ solution-code-hibernate-hb-04-map-collections-sortedset/src/main/java/com/luv2code/hibernate/demo/GetStudentImagesDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 
 import com.luv2code.hibernate.demo.entity.Student;
 
-public class GetStudentImagesSortedDemo {
+public class GetStudentImagesDemo {
 	
 	public static void main(String[] args) {
 		
