7537df2332cb7b7a9113eea60b7926ac4ceb6e80
Igor <igor.ptz@hotmail.com>
2022-01-10 21:51:45 -0300

    Update

--- solution-code-hibernate-hb-inheritance-04-mappedsuperclass/src/main/java/com/luv2code/hibernate/demo/entity/User.java
+++ solution-code-hibernate-hb-inheritance-04-mappedsuperclass/src/main/java/com/luv2code/hibernate/demo/entity/User.java
@@ -1,20 +1,12 @@
 package com.luv2code.hibernate.demo.entity;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.Table;
+import javax.persistence.MappedSuperclass;
 
-@Entity
-@Table(name="user")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="USER_TYPE", discriminatorType=DiscriminatorType.STRING)
+@MappedSuperclass
 public abstract class User {
 
 	@Id
